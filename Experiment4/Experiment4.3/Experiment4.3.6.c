a5a98910ef937133db451fd487de3d63a45c51c4
Harsh <154446476+Afkharsh@users.noreply.github.com>
2025-10-28 16:44:56 +0530

    Experiment4.3.6.c

---
+++ Experiment4/Experiment4.3/Experiment4.3.6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+ int rows = 5;
+ int i, j;
+ printf("Name- HARSH\nSAP ID - 590028742\nCourse- BCA\nBatch- B6");
+ printf("\n-----------------\n");
+ for(i = rows; i >= 1; i--) {
+  for(j = i; j >= 1; j--) {
+   printf("%d", j);
+  }
+  printf("\n");
+ }
+ return 0;
+}
