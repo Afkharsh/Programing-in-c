10384ef1aedfd5736812fa17356a6a50b224ed36
Harsh <154446476+Afkharsh@users.noreply.github.com>
2025-10-28 16:22:23 +0530

    Experiment3.5.c

---
+++ Experiment3/Experiment3.5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>  
+int main(){  
+printf("Name- HARSH\nSAP_ID-590028742\nCourse-BCA\nBatch-6");  
+printf("\n--------------------------------------\n");  
+int year,ypassed,lyears;  
+printf("enter year to be checked:");  
+scanf("%d",&year);  
+ypassed=year-1;  
+lyears=((ypassed/4)-(ypassed/100)+(ypassed/400));  
+int tdays=ypassed+lyears;  
+int daysindex=tdays%7;  
+    switch (daysindex)  
+    {  
+    case 0:  
+        printf("It was a Monday\n");  
+        break;  
+      
+    case 1:  
+        printf("It was a Tuesday\n");              
+        break;  
+    case 2:  
+        printf("It was a Wednesday\n");              
+        break;  
+    case 3:  
+        printf("It was a Thursday\n");              
+        break;  
+    case 4:  
+        printf("It was a Friday\n");              
+        break;  
+    case 5:  
+        printf("It was a Saturday\n");              
+        break;  
+    case 6:  
+        printf("It was a Sunday\n");              
+        break;  
+    default:  
+        break;  
+}  
+} 
