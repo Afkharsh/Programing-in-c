ab6e5e49c2000aa27a32f4c6c993e1656465c0f7
Harsh <154446476+Afkharsh@users.noreply.github.com>
2025-10-28 16:17:15 +0530

    Experiment3.1.c

---
+++ Experiment3/Experiment3.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main(){ 
+    printf ("Name- HARSH\nSAP_ID-590028742\nCourse-BCA\nBatch-6"); 
+    printf("\n--------------------------------------\n"); 
+    int a; 
+    printf("enter the value of digit : "); 
+    scanf("%d",&a); 
+     if (a % 2 == 0) { 
+        printf("%d is an even number.\n", a); 
+    }else { 
+        printf("%d is an odd number.\n", a);} 
+    return 0; 
+}
